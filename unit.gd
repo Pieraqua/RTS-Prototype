26afa231e98835a2cd7119dd8671ad659d0efecd
pieraqua <pauloffxii@hotmail.com>
2026-03-08 12:00:19 -0300

    feat(27): Basic Navigation pathfinding

--- unit.gd
+++ unit.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 class_name Unit
 @export var SPEED = 500
+@onready var nav = $NavigationAgent2D
 
 var animated_sprite_2d : AnimatedSprite2D
 var target_pos : Vector2
@@ -11,16 +12,37 @@ func _ready():
 	target_pos = global_position
 
 func _physics_process(_delta):
-	if walking:
-		velocity = (target_pos - global_position).normalized() * SPEED
-		move_and_slide()
-	
-	if (target_pos - global_position).length() < SPEED*_delta:
-		walking = false
-		animated_sprite_2d.stop()
-		
+	_move()
 
 func walk_towards(pos : Vector2):
 	target_pos = pos
+	nav.target_position = pos
 	walking = true
 	animated_sprite_2d.play('default')
+
+func _move() -> void:
+	
+	if nav.is_navigation_finished():
+		walking = false
+		animated_sprite_2d.stop()
+		return
+	
+	# Get pathfinding information
+	var current_agent_position: Vector2 = global_position
+	var next_path_position: Vector2 = nav.get_next_path_position()
+
+	# Calculate the new velocity
+	var new_velocity = current_agent_position.direction_to(next_path_position) * SPEED
+
+	# Set correct velocity
+	if nav.avoidance_enabled:
+		nav.set_velocity(new_velocity)
+	else:
+		_velocity_computed(new_velocity)
+
+	# Do the movement
+	move_and_slide()
+
+func _velocity_computed(safe_velocity: Vector2):
+	velocity = safe_velocity
+	
